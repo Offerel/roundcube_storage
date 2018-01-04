28c609d75ab94eb42ead692d25778c0364d116c7
Offerel <github@actrap.pfohlnet.de>
2018-01-04 20:31:37 +0100

    Small fix for Classic Skin

--- storage.php
+++ storage.php
@@ -3,7 +3,7 @@
  * Roundcube elfinder Plugin
  * Integrate elFinder in to Roundcube
  *
- * @version 1.1.0
+ * @version 1.2.0
  * @author Offerel
  * @copyright Copyright (c) 2017, Offerel
  * @license GNU General Public License, version 3
@@ -16,13 +16,9 @@ class storage extends rcube_plugin
 	{
 		$rcmail = rcmail::get_instance();
 		$this->load_config();
-
 		$this->add_texts('localization/', true);
-
 		$this->include_stylesheet($this->local_skin_path() . '/elfinder.css');
-
 		$this->include_script('client.js');
-
 		$this->register_task('storage');
 
 		$this->add_button(array(
@@ -36,10 +32,13 @@ class storage extends rcube_plugin
 		if ($rcmail->task == 'storage') {
 			$this->register_action('index', array($this, 'action'));
 		}
-		
-		$this->add_hook('template_container',                 array($this, 'add_saveatt_link'));
+
+		$this->add_hook('template_container', array($this, 'add_saveatt_link'));
 
 		$this->register_action('save_one', array($this, 'save_one'));
+		$this->register_action('elattach', array($this, 'attach_file'));
+		$rcmail->output->set_env('spath', dirname($rcmail->config->get('storage_url', false))."/");
+		$rcmail->output->set_env('elbutton', $this->gettext('loadattachment'));
 	}
 	
 	public function add_saveatt_link($p)
@@ -70,7 +69,7 @@ class storage extends rcube_plugin
 		}
 		
 		$uid = rcube_utils::get_input_value('_uid', rcube_utils::INPUT_POST);
-		$mbox    = rcube_utils::get_input_value('_mbox', rcube_utils::INPUT_POST);
+		$mbox = rcube_utils::get_input_value('_mbox', rcube_utils::INPUT_POST);
 		$mime_id = rcube_utils::get_input_value('_part', rcube_utils::INPUT_POST);
 		
 		$message = new rcube_message($uid, $mbox);
@@ -109,5 +108,92 @@ class storage extends rcube_plugin
 		$attrib['name'] = $attrib['id'];
 		return $rcmail->output->frame($attrib);
 	}
+
+	function attach_file()
+	{
+		$rcmail = rcmail::get_instance();
+
+		$uploadid = rcube_utils::get_input_value('_tid', rcube_utils::INPUT_POST);
+		$filepath = rcube_utils::get_input_value('_file', rcube_utils::INPUT_POST);
+		$COMPOSE_ID = rcube_utils::get_input_value('_cid', rcube_utils::INPUT_POST);
+		$COMPOSE = null;
+
+		if ($COMPOSE_ID && $_SESSION['compose_data_'] . $COMPOSE_ID)
+			{
+				$SESSION_KEY = 'compose_data_' . $COMPOSE_ID;
+				$COMPOSE =& $_SESSION[$SESSION_KEY];
+			}
+
+		if (!$COMPOSE) {
+		  die("Invalid session var!");
+		}
+
+		$elpath = $rcmail->config->get('storage_basepath', false).$rcmail->user->get_username().'/files'.substr($filepath, strpos($filepath, "/"));
+		$rcmail->output->reset();
+
+		if (is_file($elpath)) {
+			$temp_dir = $rcmail->config->get('temp_dir');
+			$tmpfname = tempnam($temp_dir, 'rcmAttmnt');
+			copy($elpath, $tmpfname);
+
+			$attachment = array(
+              'path' => $tmpfname,
+              'size' => filesize($elpath),
+              'name' => basename($elpath),
+              'mimetype' => rcube_mime::file_content_type($elpath, basename($elpath)),
+              'group' => $COMPOSE_ID,
+            );
+
+			$attachment = $rcmail->plugins->exec_hook('attachment_save', $attachment);
+			$id = $attachment['id'];
+
+			$COMPOSE['attachments'][$id] = $attachment;
+
+			$rcmail->session->append($SESSION_KEY.'.attachments', $id, $attachment);
+
+			if (($icon = $COMPOSE['deleteicon']) && is_file($icon))
+			{
+				$button = html::img(array(
+					'src' => $icon,
+					'alt' => $rcmail->gettext('delete')
+				));
+			}
+			else {
+				$button = rcube::Q($rcmail->gettext('delete'));
+			}
+
+			$link_content = sprintf('%s <span class="attachment-size"> (%s)</span>',
+				rcube::Q($attachment['name']), $rcmail->show_bytes($attachment['size']));
+
+			$content_link = html::a(array(
+					'href'    => "#load",
+					'class'   => 'filename',
+					'onclick' => sprintf("return %s.command('load-attachment','rcmfile%s', this, event)", rcmail_output::JS_OBJECT_NAME, $id),
+				), $link_content);
+
+			$delete_link = html::a(array(
+				'href' => "#delete",
+				'onclick' => sprintf("return %s.command('remove-attachment','rcmfile%s', this, event)", rcmail_output::JS_OBJECT_NAME, $id),
+				'title' => $rcmail->gettext('delete'),
+				'class' => 'delete',
+				), $button);
+
+			$content = $COMPOSE['icon_pos'] == 'left' ? $delete_link.$content_link : $content_link.$delete_link;
+
+			$rcmail->output->command('add2attachment_list', "rcmfile$id", array(
+              'html' => $content,
+              'name' => $attachment['name'],
+              'mimetype' => $attachment['mimetype'],
+              'classname' => rcube_utils::file2class($attachment['mimetype'], $attachment['name']),
+              'complete' => true), $uploadid);
+
+            $rcmail->output->command('auto_save_start', false);
+            $rcmail->output->send('iframe');         
+
+        } else {
+            $rcmail->output->show_message("\"$filepath\" is not a file", 'error');
+            $rcmail->output->send('iframe');
+        }
+    }
 }
 ?>
